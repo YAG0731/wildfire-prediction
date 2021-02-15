8ebc5d708a0a260dcb455b77873aef881643a4a4
SukhvirS <sukhvir23singh@gmail.com>
2021-02-15 13:03:13 -0800

    wildfire detection works now

--- src/Monitoring.js
+++ src/Monitoring.js
@@ -263,6 +263,30 @@ class Monitoring extends React.Component{
             header = <h1 className='mt-2'>Fire <a style={{color:'grey'}} onClick={this.changeView.bind(this, 'Monitoring')}>Monitoring / </a>Detection</h1>
         }
 
+        var styles = {
+            buttonGroupButton: {
+                width: '20%',
+                backgroundColor: '#f0f0f0', 
+                border: '1px solid grey',
+                padding: '10px 24px', 
+                float: 'left',
+                margin:'0 20px 0 0',
+                borderRadius: '20px',
+                color:'black',
+                outline:'none'
+            },
+            buttonGroupButtonActive: {
+                width: '20%',
+                backgroundColor: '#1580fb', 
+                border: '1px solid #1580fb',
+                color: 'white', 
+                padding: '10px 24px', 
+                float: 'left',
+                margin:'0 20px 0 0',
+                borderRadius:'20px',
+                outline:'none'
+            }
+        }
 
         return(
             <div>
@@ -271,16 +295,22 @@ class Monitoring extends React.Component{
                 <div style={{marginLeft:'15rem'}}>
 
                     <div style={{position:'fixed', backgroundColor:'#f8f9fa', height:"72px", width:"100%",  borderLeft:'1px solid #d9dadb', borderBottom:"1px solid #d9dadb", paddingLeft:"20px"}}>
-                        {/* <h1 className='mt-2'>Fire Monitoring / Detection</h1> */}
-                        {header}
+                        <h1 className='mt-2'>Fire Monitoring / Detection</h1>
+                        {/* {header} */}
                     </div>
 
                     {
                         this.state.currentView == 'Monitoring'?
                         
                         <div style={{wdith:'60vw', position:'absolute', marginTop:'72px', zIndex:'-100'}}>
+                            <div style={{width:"100%", display:'flex', justifyContent:'center', flexWrap:'wrap', margin:'10px'}}>
+                                <button style={styles.buttonGroupButtonActive}>Monitoring</button>
+                                <button style={styles.buttonGroupButton} onClick={this.changeView.bind(this, 'Detection')}>Detection</button>
+                            </div>
+                            <hr style={{margin:'0px'}}/>
+
                             <div style={{width:"100%"}}>
-                                <Map style={{height:'calc(100vh - 72px)', width:'calc(100vw - 500px)', float:'left'}} zoom={8} center={[this.state.latitude, this.state.longitude]}>
+                                <Map style={{height:'calc(100vh - 140px)', width:'calc(100vw - 500px)', float:'left'}} zoom={8} center={[this.state.latitude, this.state.longitude]}>
 
                                     <LayersControl position="topright">
 
@@ -421,7 +451,9 @@ class Monitoring extends React.Component{
                         :
                         <div>
                             {/* <ImageDetection /> */}
-                            <WildfireDetection />
+                            <WildfireDetection 
+                                viewChange = {this.changeView}
+                            />
                         </div>
                     }
                 </div>
--- src/WildfireDetection.js
+++ src/WildfireDetection.js
@@ -8,15 +8,78 @@ import DetectionImage from './DetectionComponents/DetectionImage.js';
 
 
 class WildfireDetection extends React.Component {
-  state = {
-    selectedFireImg: null,
-    bounding_boxes: [],
-    detection_scores: [],
-    detection_classes: [],
-    selectedFile: null,
-    loading: false,
-    inputFileUrl: null,
-  };
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      selectedFireImg: null,
+      bounding_boxes: [],
+      detection_scores: [],
+      detection_classes: [],
+      selectedFile: null,
+      loading: false,
+      inputFileUrl: null,
+      area: 'North California',
+      imageColor: 'True Color Composite',
+      date: null,
+      loading: true,
+    };
+
+    this.getFile = this.getFile.bind(this)
+
+  }
+
+  async componentDidMount(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
+
+    this.setState({
+      date: today
+    })
+
+    setTimeout(() => { this.getFile(); }, 10);
+
+  }
+
+  async getFile(){
+    this.setState({
+      loading: true,
+    })
+
+    var url = 'https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&&CRS=EPSG:4326&WRAP=DAY&LAYERS='    
+    var height = 800
+    if(this.state.imageColor === 'True Color Composite'){
+      url += 'MODIS_Terra_CorrectedReflectance_TrueColor'
+    }
+    else{
+      url += 'MODIS_Terra_CorrectedReflectance_Bands721'
+    }
+    url += '&FORMAT=image/jpeg&HEIGHT='+height+'&WIDTH='+height+'&BBOX='
+
+    if(this.state.area === 'North California'){
+      url += '37,-125,42,-120&TIME='
+    }
+    else{
+      url += '32,-122,39,-114&TIME='
+    }
+    url += this.state.date
+
+    const res = await fetch(url)
+    console.log(res)
+    const blob = await res.blob()
+    const file = new File([blob], 'image.jpg', {type: blob.type});
+    console.log(file)
+
+    this.setState({
+      selectedFile: file,
+      inputFileUrl: url,
+      loading: false
+    })
+  }
 
   onFileChange = (event) => {
     // Update the state
@@ -29,6 +92,9 @@ class WildfireDetection extends React.Component {
 
   // on file upload (click the upload button)
   onFileUpload = () => {
+    console.log('running onFileUpload')
+    window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);  // scroll to bottom automatically
+
     // Request made to the backend api
     // Call to fire detection API
     this.setState({ loading: true });
@@ -41,6 +107,7 @@ class WildfireDetection extends React.Component {
   // Makes a call to prediction api results
   // which include detection boxes, scores, and classifications
   async detectScore() {
+    console.log('running detect score')
     // Fetch request to wpp module
     fetch('https://wpp-fire-detection-ml.herokuapp.com/result', {
       method: 'POST'
@@ -113,6 +180,8 @@ class WildfireDetection extends React.Component {
 
   // Comsumes the ReadableStream from the Fetch call
   async detectFire() {
+    console.log('running detect fire')
+
     const formData = new FormData();
     formData.append('file', this.state.selectedFile);
 
@@ -124,6 +193,8 @@ class WildfireDetection extends React.Component {
     .then(res => {
       // Start loading
       //this.setState({ loading: true });
+
+      console.log(res)
 
       const reader = res.body.getReader();
       if(!res.ok) {
@@ -167,6 +238,31 @@ class WildfireDetection extends React.Component {
   }
 
   render() {
+    var styles = {
+      buttonGroupButton: {
+          width: '20%',
+          backgroundColor: '#f0f0f0', 
+          border: '1px solid grey',
+          padding: '10px 24px', 
+          float: 'left',
+          margin:'0 20px 0 0',
+          borderRadius: '20px',
+          color:'black',
+          outline:'none'
+      },
+      buttonGroupButtonActive: {
+          width: '20%',
+          backgroundColor: '#1580fb', 
+          border: '1px solid #1580fb',
+          color: 'white', 
+          padding: '10px 24px', 
+          float: 'left',
+          margin:'0 20px 0 0',
+          borderRadius:'20px',
+          outline:'none'
+      }
+    }
+
     return (
       <div>
         <div>
@@ -174,42 +270,58 @@ class WildfireDetection extends React.Component {
             <div style={{margin:'10px 0 0 20px', width:'75vw'}}>
 
               {/* <h1>Wildfire Detection</h1> */}
+              <div style={{width:"100%", display:'flex', justifyContent:'center', flexWrap:'wrap', margin:'10px'}}>
+                  <button style={styles.buttonGroupButton} onClick={this.props.viewChange.bind(this, 'Monitoring')}>Monitoring</button>
+                  <button style={styles.buttonGroupButtonActive}>Detection</button>
+              </div>
+              <hr style={{margin:'0px'}}/>
               
-              <div style={{width:'60%', float:'left'}}>
-              
-                <div style={{border:'1px solid grey', borderRadius:'10px', padding:'10px'}}>
+              <div style={{margin:'10px'}}>
+
+                <div style={{display:'flex', justifyContent:'center'}}>
+                  <select style={{padding:'16px'}} onChange={(e)=>{this.setState({area: e.target.value}); setTimeout(()=>{ this.getFile()}, 10) }}>
+                    <option value='North California'>North California</option>
+                    <option value='South California'>South California</option>
+                  </select>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                  <select style={{padding:'16px'}} onChange={(e)=>{this.setState({imageColor: e.target.value}); setTimeout(() =>{this.getFile()}, 10) }}>
+                    <option value='True Color Composite'>True Color Composite</option>
+                    <option value='False Color Composite'>False Color Composite</option>
+                  </select>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                  <input type='date' onChange={(e)=>{this.setState({date: e.target.value}); setTimeout(()=>{this.getFile()}, 10)}}/>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                  <button className='btn btn-primary' onClick={this.onFileUpload}>Run detection</button>
+
+                </div>
+                <hr/>
+
+                <div style={{border:'1px solid grey', borderRadius:'10px', padding:'10px', width:'60%'}}>
                   <h5 style={{fontWeight:'bold'}}>Input Image:</h5>
                   <hr/>
-                  <input type='file' onChange={this.onFileChange} />
-                  <br/>
-                  <br/>
-                  <button className='btn btn-primary' onClick={this.onFileUpload}>Run detection</button>
+                  {this.state.area}, {this.state.imageColor}, {this.state.date}:
                   <br/>
                   <br/>
 
                   {
-                    this.state.selectedFile === null?
-                    <div></div>
+                    this.state.loading === true?
+                    <div>Loading...</div>
                     :
                     <img src={this.state.inputFileUrl} width='100%' />
                   }
                 </div>
-
                 <br/>
-
-                <div style={{border:'1px solid grey', borderRadius:'10px', padding:'10px'}}>
+                
+                <div style={{border:'1px solid grey', borderRadius:'10px', padding:'10px', width:'60%', float:'left', marginBottom:'40px'}}>
                   <h5 style={{fontWeight:'bold'}}>Output Image</h5>
                   <hr/>
                   <DetectionImage url={this.state.fireImgUrl} loading={this.state.loading} />
-
                 </div>
 
-              </div>
-
-
-              <div style={{width:'35%', float:'right'}}>
-
-                <div style={{border:'1px solid grey', borderRadius:'10px', padding:'10px'}}>
+                <div style={{border:'1px solid grey', borderRadius:'10px', padding:'10px', width:'38%', float:'right'}}>
                   <h5 style={{fontWeight:'bold'}}>Detection List</h5>
                   <hr/>
                   <DetectionList
@@ -220,7 +332,6 @@ class WildfireDetection extends React.Component {
                 </div>
 
               </div>
-
             </div>
           </div>
         </div>
